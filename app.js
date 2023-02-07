98d31207a42d73267a2f40bb03243f2b73ed1ac3
JAYUN <s2115@e-mirim.hs.kr>
2023-02-07 16:10:31 +0900

    const and let

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const a = 5;
 const b = 2;
-
-const myName = "alicia";
+let myName = "alicia";
+/*
+const-값 지정후 변경x
+let-언제나 값 변경 가능
+var-사용할수 있지만 되도록이면 const,let 사용
+*/
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "alicia Choi";
+
+console.log("Your new name is " + myName);
