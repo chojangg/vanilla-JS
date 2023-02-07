49c62fa49cb7550246fd4f19ed42e02540defa8a
JAYUN <s2115@e-mirim.hs.kr>
2023-02-07 16:21:46 +0900

    Booleans

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "alicia";
-/*
-const-값 지정후 변경x
-let-언제나 값 변경 가능
-var-사용할수 있지만 되도록이면 const,let 사용
-*/
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "alicia Choi";
-
-console.log("Your new name is " + myName);
+const amIFat = null; // null==nothing
+let something; //undefined 공간은 있지만 값이 없음
+console.log(something, amIFat);
