c3116ec401bed5d8b836299b5132e764c5ec5f2f
JAYUN <s2115@e-mirim.hs.kr>
2023-02-13 17:13:29 +0900

    more events

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const title = document.querySelector("div.hello:first-child h1");
 
+console.dir(title);
+
 function handleTitleClick() {
   title.style.color = "blue";
-  console.log("title was clicked!");
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
 
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone"
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("All GOOD");
+}
+
+title.onClick = handleTitleClick;
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowONline);
