acdfbfa9bad7570160b8dc49b44c6235abf0bc22
JAYUN <s2115@e-mirim.hs.kr>
2023-02-13 15:31:43 +0900

    Events

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log();
-
-if (isNaN(age)) {
-  console.log("Please write number");
-} else if (age < 18) {
-  console.log("Your too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age > 80) {
-  console.log("You can do whatever you want");
+function handleTitleClick() {
+  title.style.color = "blue";
+  console.log("title was clicked!");
 }
+
+title.addEventListener("click", handleTitleClick);
+
