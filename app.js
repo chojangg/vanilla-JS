f94b03d4e36ef689d70160e1dd33496f58a5865b
JAYUN <s2115@e-mirim.hs.kr>
2023-02-14 17:42:57 +0900

    css in js2

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-  h1.style.color = newColor;
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
-
-
-/*
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick() {
-  if (h1.style.color === "blue") {
-    h1.style.color = "tomato";
-  } else {
-    h1.style.color = "blue";
-  }
-}
-
-h1.addEventListener("click", handleTitleClick);
-*/
